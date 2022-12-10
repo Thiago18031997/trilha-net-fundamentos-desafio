58c4797b666414ec7175e8033d92a71782d22f93
Thiago18031997 <thiagosantossouza92@gmail.com>
2022-12-10 20:34:31 -0300

    Ajuste final na parte da remoção

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -27,24 +27,22 @@ class Estacionamento
         Console.WriteLine("Escolha um dos veículos acima para remover!");
         Console.WriteLine("Digite a placa do veículo para remover:");
         string placa = Console.ReadLine().ToLower();
-        Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
-        decimal quantidadeHoras = decimal.Parse(Console.ReadLine());
-        
         for (int i = this.veiculos.Count - 1; i >= 0; i--)
         {
             
             if (this.veiculos[i].placa == placa)
             {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
+                decimal quantidadeHoras = decimal.Parse(Console.ReadLine());
                 Console.WriteLine("Removendo veículo...");
                 this.veiculos.RemoveAt(i);
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de R$: {quantidadeHoras * this.precoPorHora + this.precoInicial}");
+                break;
+            }else if(i == 0){
+                Console.WriteLine("Veículo não encontrado! Tente novamente.");
             }
-            else
-            {
-                Console.WriteLine("Veículo não encontrado, tente novamente.");
-            }
-            
         }
-        Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de R$: {quantidadeHoras * this.precoPorHora + this.precoInicial}");
+       
     }                                                                                                                                           
     //O (n)
     public void ListarVeiculos()
